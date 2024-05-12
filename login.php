9b62760accb1bb26acd59c0557c59cebf2215c74
rsaida <137534309+rsaida@users.noreply.github.com>
2024-05-12 19:57:55 +0300

    login css

--- login.php
+++ login.php
@@ -5,23 +5,81 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Document</title>
      <style>
+         
           body {
-               background-color:lightblue;
+               background-color:#FFFDF6;
+               font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
           }
-          #login{
+          #logindiv{
+               margin:auto;
                background-color:white;
+               width: 40%;
+               text-align: center;
+               border-radius: 2px;
+               box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
           }
+          #logindiv * {
+               align-items: center;
+               display: block;
+               width: 90%;
+          }
+          #email,#password{
+               height: 35px;
+               margin: 40px 50px;
+               border-radius: 2px;
+               border: 1px solid darkgray;
+               text-indent: 10px;
+          }
+          #loginbtn {
+               margin: 40px 130px 0px;
+               width: 300px;
+               height: 40px;
+               border-radius: 50px;
+               border: 1px solid lightgray;
+               background-color: #608F6B;
+               font-weight: bold;
+               font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+               color:white;
+          }
+          h1{
+               margin-bottom: 30px;
+               margin-top: 120px;
+               text-align: center;
+               color: #DC9D23;
+          }
+
+          /* #p {
+               border: 1px solid red;
+               margin: 10px 50px;
+               text-align: center;
+          } */
+          #p {
+               margin: 20px 190px;
+               text-align: center;
+               white-space: nowrap; 
+               text-align: center;
+               width: 20px;
+               height: 40px;
+          }
+
+        #p a {
+               display: inline-block;
+          }
+          span{ color: #40674A;}
+
      </style>
 </head>
 <body>
-     <h1>LOGIN</h1>
-     <div id="login">
+     <h1>Bil<span>Grocer</span></h1>
+     <div id="logindiv">
           <form method="post">
-               <input type="text" name="email" id="" placeholder="E-MAIL" value="<?= $email ?? '' ?>"> <br>
-               <input type="text" name="password" id="" placeholder="PASSWORD" value="<?= $password ?? '' ?>"> <br>
-               <button type="submit">LOGIN</button>
-               <p>New to BilFood? <a href="register.php">Join now</a> </p>
+               <br>
+               <input type="text" name="email" id="email" placeholder="E-MAIL" value="<?= $email ?? '' ?>"> 
+               <input type="text" name="password" id="password" placeholder="PASSWORD" value="<?= $password ?? '' ?>"> 
+               <input type="button" value="LOG IN" id="loginbtn">
+               <p id="p">New to BilGrocer? <a href="register.php">  Join now</a></p>
           </form>
+
      </div>
 </body>
 </html>
